321bc2a7b1f9db4de5b248842116b24536e89a74
Tetiana Zotova <zotova.tetiana@gmail.com>
2021-04-27 17:49:36 +0300

    Start

--- src/components/GoodsItem/GoodsItem.jsx
+++ src/components/GoodsItem/GoodsItem.jsx
@@ -22,7 +22,6 @@ export const GoodsItem = () => (
     </div>
     <footer class="card-footer">
       <a href="#" class="card-footer-item">Save</a>
-      <a href="#" class="card-footer-item">Edit</a>
       <a href="#" class="card-footer-item">Delete</a>
     </footer>
   </div>
